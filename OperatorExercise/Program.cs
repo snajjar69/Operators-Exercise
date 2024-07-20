25b0e4af847e618e0aaf1463af0b1215bce8827e
snajjar69 <snajjar69@gmail.com>
2024-07-20 15:34:14 -0400

    added Operator Exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,36 @@
 {
     public class Program
     {
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        public static void PrintAreaOfCircle()
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"This area of a circle with a radius of {radius}");
+            
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int subtraction = a - b;
+            int mult = a + b;
+            
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"name: {a}/{b} is {quotient} remainder {remainder}");
+          
+            
+            //AreaOfCirle
+           
         }
     }
 }
